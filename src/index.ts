dac8502abdd2cc8f81ea567349e54c56c2d131c6
aelbore <aelbore.sg@gmail.com>
2019-06-07 00:13:21 +0800

    update aria vue

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
-import * as path from 'path'
-import { createKarmaConfig, KarmaOptions } from 'aria-test'
-import { alias } from 'aria-build'
+import { KarmaOptions} from 'aria-test'
+import { createVueKarmaConfig } from './config'
 
-const vue = require('rollup-plugin-vue')
-const paths = [ 'node_modules', 'vue', 'dist', 'vue.js' ]
+export * from './config'
 
 export default function vueKarmaConfig(options?: KarmaOptions) {
-  return createKarmaConfig({ 
-    frameworks: ['mocha', 'chai'],
-    rollup: {
-      custom: true,
-      plugins: [
-        alias({ vue: path.join(...paths) }),
-        vue()
-      ]
-    },
-    ...(options || {})
-  })
+  return config => createVueKarmaConfig(options)
 }
