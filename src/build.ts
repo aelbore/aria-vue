552d028173daf2b0b269b04766eff02533ade497
aelbore <aelbore.sg@gmail.com>
2020-03-06 10:10:10 +0800

    refactor code to support new aria tools

---
+++ src/build.ts
@@ -0,0 +1,16 @@
+import { TSRollupConfig, rollupBuild, createTSRollupConfig, replacePlugin } from 'aria-build'
+
+const vue = require('rollup-plugin-vue')
+
+export function build(options: TSRollupConfig | Array<TSRollupConfig>) {
+  const configs = Array.isArray(options) ? options: [ options ]
+  return Promise.all(configs.map(config => {
+    const opts = createTSRollupConfig(config)
+    opts.inputOptions.plugins = [
+      ...opts.inputOptions.plugins,
+      vue(),
+      replacePlugin({ ...(config.replace ?? {}) })
+    ]
+    return rollupBuild(createTSRollupConfig(config))
+  }))
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { KarmaOptions} from 'aria-test'
-import { createVueKarmaConfig } from './config'
-
-export * from './config'
-
-export default function vueKarmaConfig(options?: KarmaOptions) {
-  return config => createVueKarmaConfig(options)
-}
+export * from './build'
