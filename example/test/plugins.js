1b4c7c0c7db569c5ed8821285b4706fa073b689d
aelbore <aelbore.sg@gmail.com>
2020-05-28 17:26:07 +0800

    add example code to play

---
+++ example/test/plugins.js
@@ -0,0 +1,17 @@
+import { config } from '@vue/test-utils'
+
+const DataTestIdPlugin = (wrapper) => {
+  function findByTestId(selector) {
+    const dataSelector = `[data-testid='${selector}']`
+    const element = wrapper.element.querySelector(dataSelector)
+    if (element) {
+      return element
+    }
+    return null
+  }
+  return {
+    findByTestId
+  }
+}
+
+config.plugins.VueWrapper.install(DataTestIdPlugin)
